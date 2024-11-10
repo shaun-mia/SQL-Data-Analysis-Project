b04e543f8f70fd25fc3bebe5050bab33cbf12f32
Shaun Mia <84870531+shaun-mia@users.noreply.github.com>
2024-11-10 13:59:25 +0600

    Add files via upload

---
+++ Pizza Sales Analysis using SQL/script.sql
@@ -0,0 +1,12 @@
+-- Retrieve the total number of orders placed.
+select count(order_id) as total_orders from orders;
+
+-- Calculate the total revenue generated from pizza sales.
+
+SELECT 
+    ROUND(SUM(order_details.quantity * pizzas.price),
+            2) AS total_sales
+FROM
+    order_details
+        JOIN
+    pizzas ON pizzas.pizza_id = order_details.pizza_id
